b132489bca6e3920e10f78bcce8f25cc64246696
borshchevik <85487261+borshchevik@users.noreply.github.com>
2024-06-22 17:50:53 +1100

    Update networks.js

--- src/networks.js
+++ src/networks.js
@@ -1,24 +1,24 @@
 module.exports = {
-    qtum: {
-        messagePrefix: '\x15Qtum Signed Message:\n',
-        bech32: 'qc',
+    borsh: {
+        messagePrefix: '\x15Borsh Signed Message:\n',
+        bech32: 'bc',
         bip32: {
             public: 0x0488b21e,
             private: 0x0488ade4
         },
-        pubKeyHash: 0x3a,
-        scriptHash: 0x32,
+        pubKeyHash: 0x19,
+        scriptHash: 0x35,
         wif: 0x80
     },
-    qtum_testnet: {
-        messagePrefix: '\x15Qtum Signed Message:\n',
-        bech32: 'tq',
+    borsh_testnet: {
+        messagePrefix: '\x15Borsh Signed Message:\n',
+        bech32: 'tb',
         bip32: {
             public: 0x043587cf,
             private: 0x04358394
         },
-        pubKeyHash: 0x78,
-        scriptHash: 0x6e,
+        pubKeyHash: 0x55,
+        scriptHash: 0x70,
         wif: 0xef
     }
 }
